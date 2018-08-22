49b873761e6af1f9de48b5b2d4eeadcd2207aa50
Luke Pighetti <lukepighetti@gmail.com>
2018-08-22 17:40:58 -0400

    First notes

--- lib/flutter_inapp_purchase.dart
+++ lib/flutter_inapp_purchase.dart
@@ -17,11 +17,17 @@ class FlutterInappPurchase {
 
   static const MethodChannel _channel = const MethodChannel('flutter_inapp');
 
+  /// Returns the platform version on `Android` and `iOS`.
+  ///
+  /// eg, `Android 5.1.1`
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
+  /// Consumes all items on `Android`.
+  ///
+  /// Particularly useful for removing all consumable items.
   static Future get consumeAllItems async {
     if (Platform.isAndroid) {
       final String result = await _channel.invokeMethod('consumeAllItems');
@@ -33,6 +39,10 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
+  /// Prepares `Android` to purchase items.
+  ///
+  /// Must be done on `Android` before purchasing.
+  /// On `iOS` this just checks if the client can make payments.
   static Future<String> get prepare async {
     if (Platform.isAndroid) {
       final String result = await _channel.invokeMethod('prepare');
@@ -45,6 +55,9 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
+  /// Retrieves products from the store on `Android` and `iOS`.
+  ///
+  /// `iOS` also returns subscriptions.
   static Future<List<IAPItem>> getProducts(List<String> skus) async {
     skus = skus.toList();
     if (Platform.isAndroid) {
@@ -71,6 +84,9 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
+  /// Retrieves subscriptions on `Android` and `iOS`.
+  ///
+  /// `iOS` also returns non-subscription products.
   static Future<List<IAPItem>> getSubscriptions(List<String> skus) async {
     skus = skus.toList();
     if (Platform.isAndroid) {
@@ -97,6 +113,10 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
+  /// Retrieves the user's purchase history on `Android` and `iOS`.
+  ///
+  /// Purchase history includes all types of products.
+  /// Identical to [getAvailablePurchases] on `iOS`.
   static Future<List<PurchasedItem>> getPurchaseHistory() async {
     if (Platform.isAndroid) {
       dynamic result1 = await _channel.invokeMethod(
@@ -123,6 +143,9 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
+  /// Get all non-consumed purchases made on `Android` and `iOS`
+  ///
+  /// This is identical to [getPurchaseHistory] on `iOS`
   static Future<List<IAPItem>> getAvailablePurchases() async {
     if (Platform.isAndroid) {
       dynamic result1 = await _channel.invokeMethod(
@@ -149,6 +172,9 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
+  /// Purchase a product on `Android` or `iOS`.
+  ///
+  /// Identical to [buySubscription] on `iOS`.
   static Future<PurchasedItem> buyProduct(String sku, {String oldSku}) async {
     if (Platform.isAndroid) {
       dynamic result =
@@ -177,6 +203,9 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
+  /// Purchase a subscription on `Android` or `iOS`.
+  ///
+  /// Identical to [buyProduct] on `iOS`.
   static Future<PurchasedItem> buySubscription(String sku,
       {String oldSku}) async {
     if (Platform.isAndroid) {
@@ -205,6 +234,9 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
+  /// Consumes a purchase on `Android`.
+  ///
+  /// No effect on `iOS`, whose consumable purchases are automatically consumed.
   static Future<String> consumePurchase(String token) async {
     if (Platform.isAndroid) {
       String result =
@@ -220,7 +252,10 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
-  /// android specific
+  /// End Play Store connection on `Android`.
+  ///
+  /// Absolutely necessary to call this when done with the Play Store.
+  /// No effect on `iOS`, whose store connection is always available.
   static Future<String> get endConnection async {
     if (Platform.isAndroid) {
       final String result = await _channel.invokeMethod('endConnection');
@@ -232,7 +267,10 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
-  /// ios specific
+  /// Buy a product without finishing the transaction on `iOS`.
+  ///
+  /// This may be useful if you are using server side reciept validation.
+  /// TODO: improve this comment
   static Future<PurchasedItem> buyProductWithoutFinishTransaction(
       String sku) async {
     if (Platform.isAndroid) {
@@ -259,6 +297,9 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
+  /// Finish a transaction on `iOS`.
+  ///
+  /// No effect on `Android`.
   static Future<String> finishTransaction() async {
     if (Platform.isAndroid) {
       return 'no-ops in android.';
@@ -270,7 +311,10 @@ class FlutterInappPurchase {
         code: Platform.operatingSystem, message: "platform not supported");
   }
 
-  /// helper function
+  /// Check if a subscription is active on `Android` and `iOS`.
+  ///
+  /// This is a quick and dirty way to check if a subscription is active.
+  /// It is highly recommended to do server-side validation for all subscriptions.
   static Future<bool> checkSubscribed({
     String sku,
     Duration duration: const Duration(days: 30),
