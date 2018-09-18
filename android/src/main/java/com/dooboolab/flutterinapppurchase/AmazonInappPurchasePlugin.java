ac050d90d1e3f1fe95362392e8f7ec3cc4b70234
Rockvole <rockvole@gmail.com>
2018-09-17 19:09:52 -0700

    Updates to onProductDataResponse for Amazon

--- android/src/main/java/com/dooboolab/flutterinapppurchase/AmazonInappPurchasePlugin.java
+++ android/src/main/java/com/dooboolab/flutterinapppurchase/AmazonInappPurchasePlugin.java
@@ -9,13 +9,23 @@ import android.util.Log;
 
 import com.amazon.device.iap.PurchasingListener;
 import com.amazon.device.iap.PurchasingService;
+import com.amazon.device.iap.model.Product;
 import com.amazon.device.iap.model.ProductDataResponse;
+import com.amazon.device.iap.model.ProductType;
 import com.amazon.device.iap.model.PurchaseResponse;
 import com.amazon.device.iap.model.PurchaseUpdatesResponse;
 import com.amazon.device.iap.model.UserDataResponse;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 
 import io.flutter.plugin.common.MethodCall;
@@ -28,16 +38,16 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 public class AmazonInappPurchasePlugin implements MethodCallHandler {
   public static Registrar reg;
   private final String TAG = "InappPurchasePlugin";
+  private PurchasingService mService;
   private Result result = null;
 
   ServiceConnection mServiceConn = new ServiceConnection() {
     @Override public void onServiceDisconnected(ComponentName name) {
-     // mService = null;
+      mService = null;
     }
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
       // DONT KNOW WHAT IS NEEDED HERE
-
     }
   };
 
@@ -86,6 +96,8 @@ public class AmazonInappPurchasePlugin implements MethodCallHandler {
         productSkus.add(skus.get(i));
       }
       PurchasingService.getProductData(productSkus);
+      PurchasingService.getUserData();
+      PurchasingService.getPurchaseUpdates(true);
 
     } else if (call.method.equals("getAvailableItemsByType")) {
       // NEED TO IMPLEMENT
@@ -105,62 +117,82 @@ public class AmazonInappPurchasePlugin implements MethodCallHandler {
   private PurchasingListener purchasesUpdatedListener = new PurchasingListener() {
     @Override
     public void onUserDataResponse(UserDataResponse userDataResponse) {
-      Log.d(TAG, "oudr");
+      Log.d(TAG, "oudr="+userDataResponse.toString());
     }
 
     @Override
-    public void onProductDataResponse(ProductDataResponse productDataResponse) {
-      Log.d(TAG, "opdr");
+    public void onProductDataResponse(ProductDataResponse response) {
+      Log.d(TAG, "opdr="+response.toString());
+      final ProductDataResponse.RequestStatus status = response.getRequestStatus();
+      Log.d(TAG, "onProductDataResponse: RequestStatus (" + status + ")");
+
+      switch (status) {
+        case SUCCESSFUL:
+          Log.d(TAG, "onProductDataResponse: successful.  The item data map in this response includes the valid SKUs");
+
+          final Map<String, Product> productData = response.getProductData();
+          //Log.d(TAG, "productData="+productData.toString());
+
+          final Set<String> unavailableSkus = response.getUnavailableSkus();
+          Log.d(TAG, "onProductDataResponse: " + unavailableSkus.size() + " unavailable skus");
+          Log.d(TAG, "unavailableSkus="+unavailableSkus.toString());
+          JSONArray items = new JSONArray();
+          try {
+            for (Map.Entry<String, Product> skuDetails : productData.entrySet()) {
+              Product product=skuDetails.getValue();
+              NumberFormat format = NumberFormat.getCurrencyInstance();
+
+              Number number;
+              try {
+                number = format.parse(product.getPrice());
+              } catch (ParseException e) {
+                result.error(TAG, "Price Parsing error", e.getMessage());
+                return;
+              }
+              JSONObject item = new JSONObject();
+              item.put("productId", product.getSku());
+              item.put("price", number.toString());
+              item.put("currency", "CAD");
+              ProductType productType = product.getProductType();
+              switch (productType) {
+                case ENTITLED:
+                  item.put("type", "inapp");
+                  break;
+              }
+              item.put("localizedPrice", product.getPrice());
+              item.put("title", product.getTitle());
+              item.put("description", product.getDescription());
+              item.put("introductoryPrice", "");
+              item.put("subscriptionPeriodAndroid", "");
+              item.put("freeTrialPeriodAndroid", "");
+              item.put("introductoryPriceCyclesAndroid", "");
+              item.put("introductoryPricePeriodAndroid", "");
+              System.err.println("opdr Putting "+item.toString());
+              items.put(item);
+            }
+            //System.err.println("Sending "+items.toString());
+            result.success(items.toString());
+          } catch (JSONException e) {
+            result.error(TAG, "E_BILLING_RESPONSE_JSON_PARSE_ERROR", e.getMessage());
+          }
+          break;
+        case FAILED:
+          result.error(TAG,"FAILED",null);
+        case NOT_SUPPORTED:
+          Log.d(TAG, "onProductDataResponse: failed, should retry request");
+          result.error(TAG,"NOT_SUPPORTED",null);
+          break;
+      }
     }
 
     @Override
     public void onPurchaseResponse(PurchaseResponse purchaseResponse) {
-      Log.d(TAG, "opr");
+      Log.d(TAG, "opr="+purchaseResponse.toString());
     }
 
     @Override
     public void onPurchaseUpdatesResponse(PurchaseUpdatesResponse purchaseUpdatesResponse) {
-      Log.d(TAG, "opudr");
+      Log.d(TAG, "opudr="+purchaseUpdatesResponse.toString());
     }
   };
-
-/*
-  private PurchasesUpdatedListener purchasesUpdatedListener = new PurchasesUpdatedListener() {
-    @Override
-    public void onPurchasesUpdated(int responseCode, @Nullable List<Purchase> purchases) {
-      Log.d(TAG, "Purchase Updated Listener");
-      Log.d(TAG, "responseCode: " + responseCode);
-
-      if (responseCode == BillingClient.BillingResponse.OK && purchases != null) {
-        Purchase purchase = purchases.get(0);
-
-        JSONObject item = new JSONObject();
-        try {
-          item.put("productId", purchase.getSku());
-          item.put("transactionId", purchase.getOrderId());
-          item.put("transactionDate", String.valueOf(purchase.getPurchaseTime()));
-          item.put("transactionReceipt", purchase.getPurchaseToken());
-          item.put("purchaseToken", purchase.getPurchaseToken());
-          item.put("dataAndroid", purchase.getOriginalJson());
-          item.put("signatureAndroid", purchase.getSignature());
-          item.put("autoRenewingAndroid", purchase.isAutoRenewing());
-        } catch (JSONException je) {
-          if (result != null) {
-            result.error(TAG, "E_BILLING_RESPONSE_JSON_PARSE_ERROR", je.getMessage());
-            result = null;
-          }
-        }
-        if (result != null) {
-          result.success(item.toString());
-          result = null;
-        }
-      } else {
-        if (result != null) {
-          result.error(TAG, "purchase error", "responseCode: " + responseCode);
-          result = null;
-        }
-      }
-    }
-  };
-  */
 }
