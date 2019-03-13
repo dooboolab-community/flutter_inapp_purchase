aa294728a4e8142b8b4fab567be39a21ef4577e8
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-03-14 02:47:34 +0900

    added test file

---
+++ test/flutter_inapp_purchase_test.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_inapp_purchase/flutter_inapp_purchase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:platform/platform.dart';
+
+void main() {
+  group('FlutterInappPurchase', () {
+    group('getProducts for Android', () {
+      final List<MethodCall> log = <MethodCall>[];
+      List<String> skus = List()..add("testsku");
+
+      List<IAPItem> result = List();
+
+      setUp(() {
+        FlutterInappPurchase(FlutterInappPurchase.private(
+            FakePlatform(operatingSystem: "android")));
+
+        FlutterInappPurchase.channel
+            .setMockMethodCallHandler((MethodCall methodCall) async {
+          log.add(methodCall);
+          return result;
+        });
+      });
+
+      test('invokes correct method', () async {
+        await FlutterInappPurchase.getProducts(skus);
+        expect(log, <Matcher>[
+          isMethodCall(
+            'getItemsByType',
+            arguments: <String, dynamic>{
+              'type': 'inapp',
+              'skus': skus,
+            },
+          ),
+        ]);
+      });
+    });
+    group('getProducts for iOS', () {
+      final List<MethodCall> log = <MethodCall>[];
+      List<String> skus = List()..add("testsku");
+
+      List<IAPItem> result = List();
+
+      setUp(() {
+        FlutterInappPurchase(
+            FlutterInappPurchase.private(FakePlatform(operatingSystem: "ios")));
+
+        FlutterInappPurchase.channel
+            .setMockMethodCallHandler((MethodCall methodCall) async {
+          log.add(methodCall);
+          return result;
+        });
+      });
+
+      test('invokes correct method', () async {
+        await FlutterInappPurchase.getProducts(skus);
+        expect(log, <Matcher>[
+          isMethodCall(
+            'getItems',
+            arguments: <String, dynamic>{
+              'skus': skus,
+            },
+          ),
+        ]);
+      });
+    });
+  });
+}
