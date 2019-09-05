0e47031bd8872d13d119d026c48d43d71525d569
rostopira <rostopiradv@gmail.com>
2019-09-05 11:42:16 +0300

    backup

--- android/src/main/java/com/dooboolab/flutterinapppurchase/FlutterInappPurchasePlugin.java
+++ android/src/main/java/com/dooboolab/flutterinapppurchase/FlutterInappPurchasePlugin.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 
 import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
@@ -12,12 +11,10 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 /** FlutterInappPurchasePlugin */
 public class FlutterInappPurchasePlugin implements MethodCallHandler {
   static AndroidInappPurchasePlugin androidPlugin;
-  static AmazonInappPurchasePlugin amazonPlugin;
   private static  Registrar mRegistrar;
 
   FlutterInappPurchasePlugin() {
     androidPlugin = new AndroidInappPurchasePlugin();
-    amazonPlugin = new AmazonInappPurchasePlugin();
   }
 
   // Plugin registration.
@@ -25,18 +22,13 @@ public class FlutterInappPurchasePlugin implements MethodCallHandler {
     mRegistrar = registrar;
     if(isPackageInstalled(mRegistrar.context(), "com.android.vending")) {
       androidPlugin.registerWith(registrar);
-    } else if(isPackageInstalled(mRegistrar.context(), "com.amazon.venezia")) {
-      amazonPlugin.registerWith(registrar);
     }
-
   }
 
   @Override
   public void onMethodCall(final MethodCall call, final Result result) {
     if(isPackageInstalled(mRegistrar.context(), "com.android.vending")) {
       androidPlugin.onMethodCall(call, result);
-    } else if(isPackageInstalled(mRegistrar.context(), "com.amazon.venezia")) {
-      amazonPlugin.onMethodCall(call, result);
     } else result.notImplemented();
   }
 
