78406375aeb6ab7853b4779570cffd6ce7faa9b5
hyochan <dooboolab@gmail.com>
2019-09-16 03:39:57 +0900

    Add [IAPPromotionObserver] cocoa touch file

---
+++ ios/Classes/IAPPromotionObserver.h
@@ -0,0 +1,16 @@
+//
+//  IAPPromotionObserver.h
+//  flutter_inapp_purchase
+//
+//  Created by dooboolab on 15/09/2019.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IAPPromotionObserver : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/Classes/IAPPromotionObserver.m
@@ -0,0 +1,12 @@
+//
+//  IAPPromotionObserver.m
+//  flutter_inapp_purchase
+//
+//  Created by dooboolab on 15/09/2019.
+//
+
+#import "IAPPromotionObserver.h"
+
+@implementation IAPPromotionObserver
+
+@end
