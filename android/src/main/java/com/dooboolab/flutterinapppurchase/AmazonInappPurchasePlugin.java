339b2bb5c1df96793c12ddac7fb679714303af1f
Marina González <marinagonzalez@mindapps.se>
2020-10-14 17:11:22 +0200

    Plugin migration to post Flutter 1.12

--- android/src/main/java/com/dooboolab/flutterinapppurchase/AmazonInappPurchasePlugin.java
+++ android/src/main/java/com/dooboolab/flutterinapppurchase/AmazonInappPurchasePlugin.java
@@ -1,5 +1,7 @@
 package com.dooboolab.flutterinapppurchase;
 
+import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
 
 import com.amazon.device.iap.PurchasingListener;
@@ -27,31 +29,38 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** AmazonInappPurchasePlugin */
 public class AmazonInappPurchasePlugin implements MethodCallHandler {
-  public static Registrar reg;
+
   private final String TAG = "InappPurchasePlugin";
   private Result result = null;
-  private static MethodChannel channel;
+  private MethodChannel channel;
+  private Context context;
+  private Activity activity;
 
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    channel = new MethodChannel(registrar.messenger(), "flutter_inapp");
-    channel.setMethodCallHandler(new FlutterInappPurchasePlugin());
-    reg = registrar;
+  public void setContext(Context context) {
+    this.context = context;
+  }
+
+  public void setActivity(Activity activity) {
+    this.activity = activity;
+  }
+
+  public void setChannel(MethodChannel channel) {
+    this.channel = channel;
   }
 
   @Override
   public void onMethodCall(final MethodCall call, final Result result) {
     this.result=result;
     try {
-      PurchasingService.registerListener(reg.context(), purchasesUpdatedListener);
+      PurchasingService.registerListener(context, purchasesUpdatedListener);
 
     } catch (Exception e) {
       result.error(call.method, "Call endConnection method if you want to start over.", e.getMessage());
@@ -87,7 +96,7 @@ public class AmazonInappPurchasePlugin implements MethodCallHandler {
       Log.d(TAG, "gaibt="+type);
       // NOTE: getPurchaseUpdates doesnt return Consumables which are FULFILLED
       if(type.equals("inapp")) {
-          PurchasingService.getPurchaseUpdates(true);
+        PurchasingService.getPurchaseUpdates(true);
       } else if(type.equals("subs")) {
         // Subscriptions are retrieved during inapp, so we just return empty list
         result.success("[]");
@@ -204,9 +213,9 @@ public class AmazonInappPurchasePlugin implements MethodCallHandler {
           Long transactionDate=date.getTime();
           try {
             JSONObject item = getPurchaseData(receipt.getSku(),
-                  receipt.getReceiptId(),
-                  receipt.getReceiptId(),
-                  transactionDate.doubleValue());
+                    receipt.getReceiptId(),
+                    receipt.getReceiptId(),
+                    transactionDate.doubleValue());
             Log.d(TAG, "opr Putting "+item.toString());
             result.success(item.toString());
             channel.invokeMethod("purchase-updated", item.toString());
--- android/src/main/java/com/dooboolab/flutterinapppurchase/FlutterInappPurchasePlugin.java
+++ android/src/main/java/com/dooboolab/flutterinapppurchase/FlutterInappPurchasePlugin.java
@@ -1,52 +1,141 @@
 package com.dooboolab.flutterinapppurchase;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 
-import io.flutter.plugin.common.MethodCall;
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** FlutterInappPurchasePlugin */
-public class FlutterInappPurchasePlugin implements MethodCallHandler {
-  static AndroidInappPurchasePlugin androidPlugin;
-  static AmazonInappPurchasePlugin amazonPlugin;
-  private static  Registrar mRegistrar;
+public class FlutterInappPurchasePlugin implements FlutterPlugin, ActivityAware {
 
-  FlutterInappPurchasePlugin() {
-    androidPlugin = new AndroidInappPurchasePlugin();
-    amazonPlugin = new AmazonInappPurchasePlugin();
-  }
+  private AndroidInappPurchasePlugin androidInappPurchasePlugin;
+  private AmazonInappPurchasePlugin amazonInappPurchasePlugin;
+  private Context context;
+  private MethodChannel channel;
 
-  // Plugin registration.
-  public static void registerWith(Registrar registrar) {
-    mRegistrar = registrar;
-    if(isPackageInstalled(mRegistrar.context(), "com.android.vending")) {
-      androidPlugin.registerWith(registrar);
-    } else if(isPackageInstalled(mRegistrar.context(), "com.amazon.venezia")) {
-      amazonPlugin.registerWith(registrar);
+  private static boolean isAndroid;
+  private static boolean isAmazon;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    context = binding.getApplicationContext();
+    isAndroid = isPackageInstalled(context, "com.android.vending");
+    isAmazon = isPackageInstalled(context, "com.amazon.venezia");
+
+    if (isAndroid) {
+      androidInappPurchasePlugin = new AndroidInappPurchasePlugin();
+      androidInappPurchasePlugin.setContext(context);
+
+      setupMethodChannel(binding.getBinaryMessenger(), androidInappPurchasePlugin);
+
+    } else if(isAmazon) {
+      amazonInappPurchasePlugin = new AmazonInappPurchasePlugin();
+      amazonInappPurchasePlugin.setContext(context);
+
+      setupMethodChannel(binding.getBinaryMessenger(), amazonInappPurchasePlugin);
     }
-
   }
 
   @Override
-  public void onMethodCall(final MethodCall call, final Result result) {
-    if(isPackageInstalled(mRegistrar.context(), "com.android.vending")) {
-      androidPlugin.onMethodCall(call, result);
-    } else if(isPackageInstalled(mRegistrar.context(), "com.amazon.venezia")) {
-      amazonPlugin.onMethodCall(call, result);
-    } else result.notImplemented();
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    if (isPackageInstalled(context, "com.android.vending")) {
+      tearDownChannel();
+    } else if(isPackageInstalled(context, "com.amazon.venezia")) {
+      tearDownChannel();
+    }
   }
 
-  public static final boolean isPackageInstalled(Context ctx, String packageName) {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    FlutterInappPurchasePlugin plugin = new FlutterInappPurchasePlugin();
+    if(isAndroid) {
+      AndroidInappPurchasePlugin androidInappPurchasePlugin = new AndroidInappPurchasePlugin();
+      androidInappPurchasePlugin.setContext(registrar.context());
+      androidInappPurchasePlugin.setActivity(registrar.activity());
+
+      plugin.setupMethodChannel(registrar.messenger(), androidInappPurchasePlugin);
+      plugin.setAndroidInappPurchasePlugin(androidInappPurchasePlugin);
+    } else if(isAmazon) {
+      AmazonInappPurchasePlugin amazonInappPurchasePlugin = new AmazonInappPurchasePlugin();
+      amazonInappPurchasePlugin.setContext(registrar.context());
+      amazonInappPurchasePlugin.setActivity(registrar.activity());
+
+      plugin.setupMethodChannel(registrar.messenger(), amazonInappPurchasePlugin);
+      plugin.setAmazonInappPurchasePlugin(amazonInappPurchasePlugin);
+    }
+  }
+
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+    if (isPackageInstalled(context, "com.android.vending")) {
+      androidInappPurchasePlugin.setActivity(binding.getActivity());
+    } else if(isPackageInstalled(context, "com.amazon.venezia")) {
+      amazonInappPurchasePlugin.setActivity(binding.getActivity());
+    }
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    if (isPackageInstalled(context, "com.android.vending")) {
+      androidInappPurchasePlugin.setActivity(null);
+      androidInappPurchasePlugin.onDetachedFromActivity();
+    } else if(isPackageInstalled(context, "com.amazon.venezia")) {
+      amazonInappPurchasePlugin.setActivity(null);
+    }
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+    onAttachedToActivity(binding);
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    onDetachedFromActivity();
+  }
+
+  private static boolean isPackageInstalled(Context ctx, String packageName) {
     try {
       ctx.getPackageManager().getPackageInfo(packageName, 0);
     } catch (PackageManager.NameNotFoundException e) {
       return false;
     }
     return true;
+  }
+
+  private void setupMethodChannel(BinaryMessenger messenger, MethodChannel.MethodCallHandler handler) {
+    channel = new MethodChannel(messenger, "flutter_inapp");
+    channel.setMethodCallHandler(handler);
+    setChannelByPlatform(channel);
+  }
+
+  private void tearDownChannel() {
+    channel.setMethodCallHandler(null);
+    channel = null;
+    setChannelByPlatform(null);
+  }
+
+  private void setChannelByPlatform(MethodChannel channel) {
+    if(isAndroid) {
+      androidInappPurchasePlugin.setChannel(channel);
+    } else if (isAmazon) {
+      amazonInappPurchasePlugin.setChannel(channel);
+    }
+  }
+
+  private void setAndroidInappPurchasePlugin(AndroidInappPurchasePlugin androidInappPurchasePlugin) {
+    this.androidInappPurchasePlugin = androidInappPurchasePlugin;
+  }
+
+  private void setAmazonInappPurchasePlugin(AmazonInappPurchasePlugin amazonInappPurchasePlugin) {
+    this.amazonInappPurchasePlugin = amazonInappPurchasePlugin;
   }
 }
