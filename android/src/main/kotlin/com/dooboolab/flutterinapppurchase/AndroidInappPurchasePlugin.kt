b2f448006300c84bc410105e277ee63b7ebf1bc0
rahim93259 <rahim@gonexttechnologies.com>
2022-07-20 17:04:46 +0530

    cca

--- android/src/main/kotlin/com/dooboolab/flutterinapppurchase/AndroidInappPurchasePlugin.kt
+++ android/src/main/kotlin/com/dooboolab/flutterinapppurchase/AndroidInappPurchasePlugin.kt
@@ -216,7 +216,7 @@ class AndroidInappPurchasePlugin internal constructor() : MethodCallHandler,
                 )
                 return
             }
-
+            android.util.Log.e(TAG, "consumeAllItems: logging ${lstPurchase.size}", )
             for (purchase in lstPurchase) {
                         val consumeParams = ConsumeParams.newBuilder()
                             .setPurchaseToken(purchase.purchaseToken)
