fe3834467d5cbabb849c75838ae68d8db30dc1ab
artem.semenov <artem.semenov@freeconferencecall.com>
2023-06-18 18:40:59 +0300

    Fixed 'java.lang.IllegalStateException: Reply already submitted' exception in MethodResultWrapper

--- android/src/main/kotlin/com/dooboolab/flutterinapppurchase/MethodResultWrapper.kt
+++ android/src/main/kotlin/com/dooboolab/flutterinapppurchase/MethodResultWrapper.kt
@@ -2,7 +2,6 @@ package com.dooboolab.flutterinapppurchase
 
 import android.os.Handler
 import io.flutter.plugin.common.MethodChannel
-import java.lang.Runnable
 import android.os.Looper
 
 // MethodChannel.Result wrapper that responds on the platform thread.
@@ -11,16 +10,29 @@ class MethodResultWrapper internal constructor(
     private val safeChannel: MethodChannel
 ) : MethodChannel.Result {
     private val handler: Handler = Handler(Looper.getMainLooper())
+    private var exhausted: Boolean = false
     override fun success(result: Any?) {
-        handler.post { safeResult.success(result) }
+        if (!exhausted) {
+            exhausted = true
+
+            handler.post { safeResult.success(result) }
+        }
     }
 
     override fun error(errorCode: String, errorMessage: String?, errorDetails: Any?) {
-        handler.post { safeResult.error(errorCode, errorMessage, errorDetails) }
+        if (!exhausted) {
+            exhausted = true
+
+            handler.post { safeResult.error(errorCode, errorMessage, errorDetails) }
+        }
     }
 
     override fun notImplemented() {
-        handler.post { safeResult.notImplemented() }
+        if (!exhausted) {
+            exhausted = true
+
+            handler.post { safeResult.notImplemented() }
+        }
     }
 
     fun invokeMethod(method: String?, arguments: Any?) {
